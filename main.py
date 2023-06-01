8cb1aeeb4e787a94ea392d93a17c1e2685516961
Ednilson Messias Castro Dos Santos <ednilson.santos@taking.com.br>
2023-06-01 09:03:22 -0300

    Refatora classe main

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import array
 from candidato import Candidato
+from urnaEletronica import UrnaEletronica
 
 # Cadastra candidatos
-candidatos = [
+presidentes = [
         Candidato(1, "Ednilson Messias", "Presidente", 99, "Pantera"),
-        Candidato(2, "Renata Gomes", "Presidente", 88, "Gavião"),
+        Candidato(2, "Renata Gomes", "Presidente", 88, "Gavião")
+    ]
+
+senadores = [
         Candidato(3, "Georgen", "Senador", 881, "Gavião"),
         Candidato(4, "Marco Antônio", "Senador", 882, "Gavião"),
         Candidato(5, "Nadilson Lima", "Senador", 991, "Pantera")
     ]
 
-votosPresidente = []
-votosSenador = []
+urna = UrnaEletronica([],[])
+
 
 print("################################")
 print("####### ELEIÇÕES ABERTAS #######")
@@ -25,14 +29,43 @@ opcao = input("Digite a opção desejada:\n")
 while opcao != "1":
     match opcao:
         case "0":
-            for candidato in candidatos:
-                print("Nome: " + candidato.nome + " (" + candidato.cargo + ") " + " Partido: " + candidato.partido + " Número: " + str(candidato.numero))
+
+            print("*** PRESIDENTES ***")
+            for presidente in presidentes:
+                presidente.print_caditado()
             
-            print("\n")
-            votosPresidente.append(input("Digite o número para PRESIDENTE:\n"))
+            votoPresidente = int(input("Digite o número para PRESIDENTE:\n"))
+
+            presidenteSelecionado = list(filter( lambda obj: obj.numero == votoPresidente, presidentes))
+
+            while(len(presidenteSelecionado) == 0):
+                print("\n")
+                print("Candidato não cadastrado.")
+                print("Selecione um novo candidato.")
+                votoPresidente = int(input("Digite o número para PRESIDENTE:\n"))
+                presidenteSelecionado = list(filter( lambda obj: obj.numero == votoPresidente, presidentes))
+                print("\n")
+
+            urna.votosPresidente.append(votoPresidente)            
             # TODO verificar se o número é um dos presidentes cadastrados, se não for pedir pra informar um número ou dar a opção de abortar (colocar em um while)
 
-            votosSenador.append(input("Digite o número para SENADOR:\n"))
+            print("*** SENADORES ***")
+            for senador in senadores:
+                senador.print_caditado()
+            
+            votoSenador = int(input("Digite o número para SENADOR:\n"))
+
+            senadorSelecionado = list(filter( lambda obj: obj.numero == votoSenador, senadores))
+
+            while(len(senadorSelecionado) == 0):
+                print("\n")
+                print("Candidato não cadastrado.")
+                print("Selecione um novo candidato.")
+                votoSenador = int(input("Digite o número para SENADOR:\n"))
+                senadorSelecionado = list(filter( lambda obj: obj.numero == votoSenador, senadores))
+                print("\n")
+
+            urna.votosSenador.append(votoSenador)
             # TODO verificar se o número é um dos senadores cadastrados, se não for pedir pra informar um número ou dar a opção de abortar (colocar em um while)
 
             print("\n")
@@ -54,13 +87,25 @@ while opcao != "1":
 
 # TODO apresentar votos sumarizados como nome dos canditados e quantos votos receberam
 
-for voto in votosPresidente:
-    print("Votos para PRESIDENTE:\n")
-    print(voto)
 print("\n")
 
-for voto in votosSenador:
-    print("Votos para SENADOR:\n")
-    print(voto)
+votosPresidenteSumarizado = {}
+print("Votos para PRESIDENTE:\n")
+for voto in urna.votosPresidente:
+    if voto in votosPresidenteSumarizado:
+        votosPresidenteSumarizado[voto] = votosPresidenteSumarizado[voto] + 1
+    else:
+        votosPresidenteSumarizado[voto] = 1
+print(votosPresidenteSumarizado)
+print("\n")
+
+votosSenadorSumarizado = {}
+print("Votos para SENADOR:\n")
+for voto in urna.votosSenador:
+    if voto in votosSenadorSumarizado:
+        votosSenadorSumarizado[voto] = votosSenadorSumarizado[voto] + 1
+    else:
+        votosSenadorSumarizado[voto] = 1
+print(votosSenadorSumarizado)
 print("\n")
 
